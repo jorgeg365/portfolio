a2d791147b7eb3f3da290de359937bb313cb7fd3
Your Name <your.email@example.com>
2026-03-24 14:26:21 -0400

    Add hover scale effect to OS icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ function OSIcons() {
     <div className="flex justify-center items-center gap-12 mt-8 mb-8">
       {/* Windows Icon */}
       <div className="flex flex-col items-center gap-3">
-        <div className="w-20 h-20 bg-blue-500 rounded-xl flex items-center justify-center">
+        <div className="w-20 h-20 bg-blue-500 rounded-xl flex items-center justify-center transition-transform duration-200 ease-out hover:scale-110">
           <svg width="40" height="40" viewBox="0 0 24 24" fill="white">
             <path d="M3 12V6.75l6-1.32v6.48L3 12zm17-9v8.75l-10 .15V5.21L20 3zM3 13l6 .09v6.81l-6-1.15V13zm17 .25V22l-10-1.91V13.1l10 .15z"/>
           </svg>
@@ -18,7 +18,7 @@ function OSIcons() {
 
       {/* Mac Icon */}
       <div className="flex flex-col items-center gap-3">
-        <div className="w-20 h-20 bg-gray-800 rounded-xl flex items-center justify-center">
+        <div className="w-20 h-20 bg-gray-800 rounded-xl flex items-center justify-center transition-transform duration-200 ease-out hover:scale-110">
           <svg width="40" height="40" viewBox="0 0 24 24" fill="white">
             <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.96-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.03-3.11z"/>
           </svg>
@@ -28,7 +28,7 @@ function OSIcons() {
 
       {/* Linux Icon */}
       <div className="flex flex-col items-center gap-3">
-        <div className="w-20 h-20 rounded-xl overflow-hidden flex items-center justify-center">
+        <div className="w-20 h-20 rounded-xl overflow-hidden flex items-center justify-center transition-transform duration-200 ease-out hover:scale-110">
           <Image
             src="/linux.png"
             alt="Linux Logo"
